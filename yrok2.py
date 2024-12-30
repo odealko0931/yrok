a2f7f98318ef6a79aca7c8dad5e336367d73efd4
odealko093 <goshankotov10@gmail.com>
2024-12-30 13:31:19 +0300

    yrok3

--- yrok2.py
+++ yrok2.py
@@ -8,9 +8,112 @@
 #ws = (14)
 #print (ty)
 #print(ws)
-a = int(input())
-b = int(input())
-print (a + b )
-print (a - b )
-print ( a * b  )
-print ( a / b  )
+#a = int(input("ввидите число" ))
+#b = int(input(" введите 2 число "))
+#c = int(input("введите 3 число"))
+#print (a + b )
+#print (a - b )
+#print ( a * b  )
+#print ( a / b  )
+#print (a + b + c)
+#print (a - b -c )
+#print ( a * b *c)
+
+#w = int(input("введите ввозраст "))
+#if (w > 18):
+#    print("вход разрещон")
+#else:
+#    print ("вход запрешен ")
+#admin
+#adoh8og3o
+
+
+#user
+#asdhioi3bo
+
+#guest
+#dfio43bkd
+
+#login = input(" логин ")
+
+#password = input(" пороль ")
+#if(login == "admin" and password == "adoh8og3o"):
+ #   print("здравствуйте админ ")
+#elif (login == "user" and password == "asdhioi3bo"):
+  #  print (" здравствуйте")
+#elif (login == "guest" and password == "dfio43bkd"):
+#    print(" здравствуйте пользователь 3 ")
+#else:
+ #   print ("login или password введены не верно")
+
+#import random
+
+#o_r = random.randint(0, 1 )
+#print( o_r )
+#if ( o_r == 0):
+ #   print("reshka")
+#else:
+ #   print("opel")
+
+#day = int(input("введите число "))
+
+#if (day == 1 ):
+#    print("понедельник")
+#elif(day == 2 ):
+ #   print ("вторник")
+#elif(day == 3):
+  #  print ("среда")
+#elif(day == 4):
+ #   print ("четверк")
+#elif(day == 5):
+   # print("пятница")
+#elif(day == 6 ):
+ #   print("суббота")
+#elif(day == 7 ):
+#    print("воскресенье")
+#else:
+#    print("число введено не верно")
+
+#a = int(input())
+#if (a == 1 or a ==2 or a ==3):
+ #   print("zima")
+#elif(a ==4 or a == 5 or a ==6):
+ #   print("fesna")
+#elif(a == 7 or a == 8 or a == 9 ):
+    #print("leto")
+#elif(a == 10 or a == 11 or a ==12 ):
+   #print("oseni")
+#e3lse:
+ #   print(" введено неправельно
+#item = 0
+#while item <101:
+ #   print(item)
+  #  item = item + 1
+
+#item = 0
+#while item <2011:
+#    print (item)
+#    item = item +1
+
+#for item in range(1,333,50):
+    #print (item)
+
+#import random
+
+#a = random.randint(1,100)
+
+#while True:
+ #   b = int(input())
+  # if (b < a ):
+   #     print ("загадочное число больше ")
+    #elif (b > a ):
+     #   print ("загадочное число меньше ")
+    #else:
+     #   print ("вы угодали ")
+      #  print (a)
+       # break
+
+for item in range (5,1001,5):
+    if(item > 500 and item <600):
+        break
+    print(item)
